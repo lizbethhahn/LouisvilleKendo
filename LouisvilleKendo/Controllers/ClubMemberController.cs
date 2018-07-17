ea4105366fd45334fd341abb1088e564ce724e9f
elizabeth hahn <kendoka69@mac.com>
2018-07-16 22:13:55 -0400

    Incorporated front-end website into MVC Web App.

---
+++ LouisvilleKendo/Controllers/ClubMemberController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LouisvilleKendo.DAL;
+using LouisvilleKendo.Models;
+
+namespace LouisvilleKendo.Controllers
+{
+    public class ClubMemberController : Controller
+    {
+        private ClubContext db = new ClubContext();
+
+        // GET: ClubMember
+        public ActionResult Index()
+        {
+            return View(db.ClubMembers.ToList());
+        }
+
+        // GET: ClubMember/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClubMember clubMember = db.ClubMembers.Find(id);
+            if (clubMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clubMember);
+        }
+
+        // GET: ClubMember/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClubMember/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,AuskfId,LastName,FirstName,StartDate,Rank,FederationMember,CurrentMember")] ClubMember clubMember)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClubMembers.Add(clubMember);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clubMember);
+        }
+
+        // GET: ClubMember/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClubMember clubMember = db.ClubMembers.Find(id);
+            if (clubMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clubMember);
+        }
+
+        // POST: ClubMember/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,AuskfId,LastName,FirstName,StartDate,Rank,FederationMember,CurrentMember")] ClubMember clubMember)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clubMember).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clubMember);
+        }
+
+        // GET: ClubMember/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClubMember clubMember = db.ClubMembers.Find(id);
+            if (clubMember == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clubMember);
+        }
+
+        // POST: ClubMember/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClubMember clubMember = db.ClubMembers.Find(id);
+            db.ClubMembers.Remove(clubMember);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- LouisvilleKendo/DAL/ClubInitializer.cs.cs
+++ LouisvilleKendo/DAL/ClubInitializer.cs.cs
@@ -11,17 +11,17 @@ namespace LouisvilleKendo.DAL
     {
         protected override void Seed(ClubContext context)
         {
-            var students = new List<ClubMember>
+            var clubMembers = new List<ClubMember>
             {
             new ClubMember{AuskfId=717,LastName="Hahn",FirstName="Elizabeth",StartDate=DateTime.Parse("2004-04-15"),Rank="3 Dan",FederationMember=true,CurrentMember=true},
             new ClubMember{AuskfId=10612,LastName="Floro",FirstName="Mariano",StartDate=DateTime.Parse("2004-04-15"),Rank="2 Dan",FederationMember=true,CurrentMember=true},
             new ClubMember{AuskfId=13615,LastName="Goldschmidt",FirstName="Eric",StartDate=DateTime.Parse("2004-04-15"),Rank="1 Dan",FederationMember=false,CurrentMember=false},
             new ClubMember{AuskfId=15607,LastName="West",FirstName="Adam",StartDate=DateTime.Parse("2004-04-15"),Rank="2 Dan",FederationMember=true,CurrentMember=true},
             new ClubMember{AuskfId=17312,LastName="Brown",FirstName="Steven",StartDate=DateTime.Parse("2004-04-15"),Rank="4 Kyu",FederationMember=true,CurrentMember=true},
-            new ClubMember{AuskfId=17313,LastName="Brown",FirstName="Faith",StartDate=DateTime.Parse("2004-04-15"),Rank="none",FederationMember=false,CurrentMember=false},
+            new ClubMember{AuskfId=17313,LastName="Brown",FirstName="Faith",StartDate=DateTime.Parse("2004-04-15"),Rank="none",FederationMember=false,CurrentMember=false}
             };
 
-            students.ForEach(s => context.ClubMembers.Add(s));
+            clubMembers.ForEach(s => context.ClubMembers.Add(s));
             context.SaveChanges();
         }
     }
